47118879900d7ce968dca31879e55db9e7b49565
isabelle9912 <isabellesaahirah28@gmail.com>
2025-01-22 15:31:22 -0300

    fix the class Utils

--- src/entites/Utils.java
+++ src/entites/Utils.java
@@ -11,23 +11,4 @@ public class Utils {
     public boolean arrayListIsEmpty(ArrayList<?> a) {
         return a == null || a.isEmpty();
     }
-
-    public byte checkInstance(Object o) {
-        if (o == null) {
-            return -1;
-        }
-        if (o instanceof Pessoa) {
-            return 1;
-        } else if (o instanceof Cliente) {
-            return 2;
-        } else if (o instanceof Atendente) {
-            return 3;
-        } else if (o instanceof Tecnico) {
-            return 4;
-        } else {
-            return -1;
-        }
-
-    }
-
 }
